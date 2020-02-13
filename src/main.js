749146a475ce77be0e8c6d480c4ec89fb93c3bab
Kevin Zhang <kevinz5383@yahoo.com>
2020-02-13 00:34:31 -0800

    broke more things

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import Home from './Home.vue'
 import Wallet from './User/Wallet.vue'
 import Storefront from './Store/Storefront.vue'
 import About from './About.vue'
+import Login from './Login.vue'
 
 Vue.use(ElementUI)
 Vue.component(VueQrcode.name, VueQrcode);
@@ -19,6 +20,7 @@ const routes = [
   { name: 'wallet', path: '/wallet', component: Wallet },
   { name: 'storefront', path: '/store/:vendorName', component: Storefront },
   { name: 'about', path: '/', component: About },
+  { name: 'login', path: '/', component: Login },
 ]
 
 const router = new VueRouter({
